5b34821707da35ddcaffab25fb7d196d0b56e0f9
Melian2214 <milena.gomez@utp.edu.co>
2026-02-24 23:31:37 -0500

    Update ejercicio1.pl

--- ejercicio1.pl
+++ ejercicio1.pl
@@ -17,6 +17,11 @@ hijo(selma, ling).
 
 padre(P, H) :- hijo(P, H).
 madre(M, H) :- hijo(M, H).
-
 abuelo(A, X) :- hijo(A, P), hijo(P, X).
 abuela(A, X) :- hijo(A, P), hijo(P, X).
+nieto(X, A) :- abuelo(A, X).
+nieto(X, A) :- abuela(A, X).
+hermanos(X, Y) :- hijo(P, X), hijo(P, Y), X \= Y.
+tio(X, Y) :- hermanos(X, P), hijo(P, Y).
+sobrino(X, Y) :- tio(Y, X).
+primos(X, Y) :- hijo(P1, X), hijo(P2, Y), hermanos(P1, P2), X \= Y.
